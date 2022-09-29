7de50bd5f46eb27fd8bb3257be7ce88f4c57a504
portuna85 <portuna85@gmail.com>
2022-09-29 23:45:41 +0900

    TypeConveter

--- src/test/java/hello/typeconverter/converter/ConverterTest.java
+++ src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.converter;
 
+import hello.typeconverter.type.IpPort;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -29,5 +30,29 @@ class ConverterTest {
 
         // then
         assertThat(result).isEqualTo("10");
+    }
+
+    @Test
+    void stringToIpPort() throws Exception {
+        // given
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort source = new IpPort("127.0.0.1", 8080);
+        // when
+        String result = converter.convert(source);
+
+        // then
+        assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+
+    @Test
+    void ipPortToString() throws Exception {
+        // given
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        String source = "127.0.0.1:8080";
+        // when
+        IpPort result = converter.convert(source);
+
+        // then
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
     }
 }
