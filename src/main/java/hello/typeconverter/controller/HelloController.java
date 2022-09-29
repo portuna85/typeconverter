4d59ebf2e59546af1ebaa97522f7935ee4914713
portuna85 <portuna85@gmail.com>
2022-09-29 22:26:19 +0900

    TypeConveter

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,6 +1,7 @@
 package hello.typeconverter.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +14,12 @@ public class HelloController {
         String data = request.getParameter("data");
         Integer intValue = Integer.valueOf(data);
         System.out.println("intValue = " + intValue);
+        return "Ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
         return "Ok";
     }
 }
